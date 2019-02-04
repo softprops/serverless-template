8645434e646cc2a8460375ad4414e8612fc006fc
softprops <d.tangren@gmail.com>
2019-02-04 01:04:51 -0500

    test cli

--- tests/index.ts
+++ tests/index.ts
@@ -1,11 +1,31 @@
 import * as chai from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
 import * as _ from 'lodash';
-import { questions } from '../src/';
+import { cli, questions } from '../src/';
 
 before(() => {
   chai.should();
   chai.use(chaiAsPromised);
+});
+
+describe('#cli', () => {
+  it('requires template', async () => {
+    return chai
+      .expect(cli([]))
+      .eventually.be.rejectedWith('Option "--template <uri>" missing');
+  });
+
+  it('requires output', async () => {
+    return chai
+      .expect(cli(['..', '..', '--template', 'uri']))
+      .eventually.be.rejectedWith('Option "--output <dir>" missing');
+  });
+
+  it('returns options provided', async () => {
+    return chai
+      .expect(cli(['..', '..', '--template', 'uri', '--output', 'dir']))
+      .eventually.deep.eq({ version: '0.1.0', template: 'uri', output: 'dir' });
+  });
 });
 
 // https://www.chaijs.com/api/bdd/
